1b50b96a8ee54b3c89aca5152758051789d9ae44
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2024-05-02 23:59:04 +0800

    categories can CRUD safely

--- app/(dashboard)/[storeid]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeid]/(routes)/categories/components/cell-action.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import axios from "axios";
 
-import { BillboardColumn } from "./columns";
+import { CategoryColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { 
@@ -18,7 +18,7 @@ import {
 import { AlertModal } from "@/components/modal/alert-modal";
 
 interface CellActionProps {
-  data: BillboardColumn;
+  data: CategoryColumn;
 };
 
 export const CellAction: React.FC<CellActionProps> = ({
@@ -32,17 +32,17 @@ export const CellAction: React.FC<CellActionProps> = ({
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success('Billboard Id copied to clipboard.')
+    toast.success('Category Id copied to clipboard.')
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`)
       router.refresh();
-      toast.success('Billboard deleted.')
+      toast.success('Category deleted.')
     } catch (error) {
-      toast.error("Make sure you removed all categories using this billboard first.");
+      toast.error("Make sure you removed all products using this category first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -72,7 +72,7 @@ export const CellAction: React.FC<CellActionProps> = ({
             <Copy className="mr-2 h-4 w-4"/>
             Copy Id
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
+          <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/categories/${data.id}`)}>
             <Edit className="mr-2 h-4 w-4"/>
             Update
           </DropdownMenuItem>
